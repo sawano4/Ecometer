ee6b0370bf07589a788c1567ba6ca663f4d7014a
OmrHz <mo_hamza@esi.dz>
2024-04-22 01:16:59 +0100

    not important

---
+++ backend/config/database.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+//database config
+
+module.exports = {
+    development: {
+        connectionString: 'mongodb+srv://moussoubillel:billel2004@sawanus.xw7ivfh.mongodb.net/?retryWrites=true&w=majority&appName=Sawanus',
+    },
+    production: {
+        connectionString: process.env.DB_URL,
+    },
+};
